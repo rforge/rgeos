5237bca300df35c2204b0b66416a7b3f2c91da73
crundel <crundel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-05-30 20:15:54 +0000

    Added RGEOSBuffer
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgeos@86 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rgeos_buffer.R
+++ pkg/R/rgeos_buffer.R
@@ -1,24 +1,55 @@
 
 # width - double, quadsegs - int, joinStyle - int, mitreLimit - double, leftSide - int
 
-RGEOSBuffer = function(spgeom, width, quadsegs) {
-    
-    id = as.character(1:10)
-    
-    x = .Call("rgeos_buffer", .RGEOS_HANDLE, spgeom, id, 0.0, width, as.integer(quadsegs), PACKAGE="rgeos")
-    return(x)
-}
-
-RGEOSBufferWithStyle = function(spgeom, width, quadsegs, endCapStyle, joinStyle, mitreLimit) {
+RGEOSBuffer = function(spgeom, byid=TRUE, id=NULL, width=1.0, quadsegs=5, 
+                       capStyle="ROUND", joinStyle="ROUND", mitreLimit=1.0) {
     
     GEOSCapStyles = c("ROUND","FLAT","SQUARE")
     GEOSJoinStyles = c("ROUND","MITRE","BEVEL")
     
+    curids = extractIDs(spgeom)
+    if (is.null(id)) {
+        if (byid)   id = curids
+        else        id = "buffer"
+    }
     
+    if (byid == TRUE  && length(id) != length(curids) )
+        stop("if applying by ids, new ids must be same length as current ids")
+    if (byid == FALSE && length(id) != 1 ) 
+        stop("if applying across ids, new id must be length 1")
     
+    id = as.character(id)
+    width = as.numeric(width)
+    quadsegs = as.integer(quadsegs)
+    byid = as.logical(byid)
+    mitreLimit=as.numeric(mitreLimit)
+    
+    if (is.character(capStyle)) 
+        capStyle = which(match.arg(toupper(capStyle),GEOSCapStyles) == GEOSCapStyles)
+    if (is.character(joinStyle)) 
+        joinStyle = which(match.arg(toupper(joinStyle),GEOSJoinStyles) == GEOSJoinStyles)
+    
+    if ( !(capStyle %in% 1:length(GEOSCapStyles)) ) stop("invalid cap style")
+    if ( !(joinStyle %in% 1:length(GEOSJoinStyles)) ) stop("invalid join style")
+    
+    capStyle= as.integer(capStyle)
+    joinStyle= as.integer(joinStyle)
+    
+    if (mitreLimit <= 0) 
+        stop("mitreLimit must be greater than 0")
+    if (capStyle == 2 && inherits(spgeom,"SpatialPoints")) 
+        stop("Flat capstyle is incompatible with SpatialPoints geometries")
+    if (width < 0 && !inherits(spgeom,"SpatialPolygons")) 
+        stop("Negative width values may only be used with SpatialPolygons geometries")
+
+    ans = .Call("rgeos_buffer", .RGEOS_HANDLE, spgeom, byid, id, 0.0, width, quadsegs,
+                                capStyle, joinStyle, mitreLimit, PACKAGE="rgeos")
+    
+    return(ans)
 }
 
-RGEOSSingleSidedBuffer = function(spgeom, width, quadsegs, joinStyle, mitreLimit, leftSide) {
+
+RGEOSSingleSidedBuffer = function(spgeom, width, quadsegs, joinStyle, mitreLimit) {
     
     GEOSCapStyles = c("ROUND","FLAT","SQUARE")
     GEOSJoinStyles = c("ROUND","MITRE","BEVEL")
--- pkg/src/rgeos_buffer.c
+++ pkg/src/rgeos_buffer.c
@@ -1,17 +1,51 @@
 #include "rgeos.h"
 
-SEXP rgeos_buffer(SEXP env, SEXP obj, SEXP id, SEXP thres, SEXP width, SEXP quadsegs) {
+SEXP rgeos_buffer(SEXP env, SEXP obj, SEXP byid, SEXP id, SEXP thres, SEXP width, SEXP quadsegs, 
+                  SEXP capStyle, SEXP joinStyle, SEXP mitreLimit) {
     
     SEXP ans, p4s;
-    GEOSGeom geom, res;
+    GEOSGeom geom, curgeom, res;
+    GEOSGeom *geoms;
+    int i, n;
     
     GEOSContextHandle_t GEOShandle = getContextHandle(env);
     
     geom = rgeos_convert_R2geos(env, obj);
     p4s = GET_SLOT(obj, install("proj4string"));
     
-    res = GEOSBuffer_r(GEOShandle, geom, NUMERIC_POINTER(width)[0], INTEGER_POINTER(quadsegs)[0]);
+    if (LOGICAL_POINTER(byid)[0])
+        n = GEOSGetNumGeometries_r(GEOShandle, geom);
+    else
+        n = 1;
     
+    geoms = (GEOSGeom *) R_alloc((size_t) n, sizeof(GEOSGeom));
+    
+    curgeom = geom;
+    for(i=0; i<n; i++) {
+        if ( n > 1) {
+            curgeom = (GEOSGeom) GEOSGetGeometryN_r(GEOShandle, geom, i);
+            if (curgeom == NULL) error("rgeos_writeWKT: unable to get subgeometries");
+        }
+        
+        geoms[i] = GEOSBufferWithStyle_r(GEOShandle, curgeom, 
+                                         NUMERIC_POINTER(width)[0], 
+                                         INTEGER_POINTER(quadsegs)[0], 
+                                         INTEGER_POINTER(capStyle)[0], 
+                                         INTEGER_POINTER(joinStyle)[0],  
+                                         NUMERIC_POINTER(mitreLimit)[0]);
+    }
+    
+    if (n == 1) {
+        res = geoms[0];
+    } else {
+        res = GEOSGeom_createCollection_r(GEOShandle, GEOS_GEOMETRYCOLLECTION, geoms, n);
+    }
+
+
     ans = rgeos_convert_geos2R(env, res, p4s, id, thres);
+
+    GEOSGeom_destroy_r(GEOShandle, geom);
+    //GEOSGeom_destroy_r(GEOShandle, res);
     return(ans);
 }
+
