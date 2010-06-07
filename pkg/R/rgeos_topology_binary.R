1ccdb325302f589529f3916e06d2d935f8756bda
crundel <crundel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-07 02:51:01 +0000

    Renamed rgeos_topology_relation to rgeos_topology_binary
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgeos@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rgeos_topology_binary.R
+++ pkg/R/rgeos_topology_binary.R
@@ -1,3 +1,4 @@
+
 RGEOSDifference = function(g1, g2) {}
 
 RGEOSSymDifference = function(g1,	g2) {}
@@ -5,5 +6,3 @@ RGEOSSymDifference = function(g1,	g2) {}
 RGEOSIntersection = function(g1, g2) {}
 
 RGEOSUnion = function(g1, g2) {}
-
-RGEOSUnionCascaded = function(g1) {}
