b81d1a695dad3c2a6415ee65d1f9ed3dc28bb2b6
crundel <crundel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-26 18:42:51 +0000

    Initial implementation of SpatialRings and SpatialRingsDataFrame classes based on SpatialLines
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgeos@118 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/rgeos_SpatialRings.R
@@ -0,0 +1,177 @@
+
+setClass("Ring", 
+    representation(coords = "matrix",ID = "character"),
+    validity = function(object) {
+        if (any(is.na(object@coords)))
+            stop("coords cannot contain missing values")
+        if (ncol(object@coords) != 2)
+            stop("coords should have 2 columns")
+        if (!all(object@coords[1,] == object@coords[nrow(object@coords),]))
+            stop("invalid ring, start and end coordinates must be equal")
+        return(TRUE)
+    }
+)
+
+
+setClass("SpatialRings",
+    representation("Spatial", rings = "list"),
+    
+    prototype = list(bbox = matrix( rep(NA, 2), 2, 2, dimnames = list(NULL, c("min","max"))),
+                                    proj4string = CRS(as.character(NA)),
+                                    rings = list()),
+    
+    validity = function(object) {
+        if (any(unlist(lapply(object@rings, function(x) !is(x, "Ring"))))) 
+            stop("rings not Ring objects")
+        if (any(duplicated(sapply(slot(object, "rings"), function(i) slot(i, "ID")))))
+            return("non-unique Rings ID slot values")
+        return(TRUE)
+    }
+)
+
+Ring <- function(coords,ID=as.character(NA)) {
+    coords <- coordinates(coords)
+    if (ncol(coords) != 2) 
+        stop("coords must be a two-column matrix")
+    new("Ring", coords = coords, ID = ID)
+}
+
+.bboxSRs <- function(lst) {
+    bb = sapply(lst, bbox)
+    res = matrix(c(min(bb[1,]), min(bb[2,]), max(bb[3,]), max(bb[4,])), 2, 2)
+    dimnames(res) = list(c("x", "y"), c("min", "max"))
+    return(res)
+}
+
+SpatialRings <- function(RingList, proj4string=CRS(as.character(NA))) {
+    if (any(sapply(RingList, function(x) !is(x, "Ring")))) 
+        stop("Ring list not exclusively filled with Ring objects")
+    Sp <- new("Spatial", bbox = .bboxSRs(RingList), proj4string=proj4string)
+    res <- new("SpatialRings", Sp, rings=RingList)
+    return(res)
+}
+
+
+bbox.Ring <- function(obj) {
+    rx <- range(obj@coords[,1])
+    ry <- range(obj@coords[,2])
+    res = rbind(r1 = rx, r2 = ry)
+    dimnames(res)[[2]] <- c("min", "max")
+    return(res)
+}
+setMethod("bbox", "Ring", bbox.Ring)
+
+
+plotSpatialRings <- function(SR, xlim = NULL, ylim = NULL,
+                             col = 1, lwd = 1, lty=1, add = FALSE, axes = FALSE, ..., 
+                             setParUsrBB=FALSE) {
+
+    if (!add) 
+        plot(as(SR, "Spatial"), xlim = xlim, ylim = ylim, axes = axes, ..., setParUsrBB=setParUsrBB)
+    
+
+    lst <- SR@rings
+    if (length(col) != length(lst)) 
+        col <- rep(col[1], length(lst))
+    if (length(lwd) != length(lst)) 
+        lwd <- rep(lwd[1], length(lst))
+    if (length(lty) != length(lst)) 
+        lty <- rep(lty[1], length(lst))
+
+    for (i in seq(along=lst)) {
+        crds <- coordinates(lst[[i]])
+        lines(crds, col = col[i], lwd = lwd[i], lty = lty[i], ...)
+    }
+}
+
+setMethod("plot", 
+          signature(x = "SpatialRings", y = "missing"), 
+          function(x, y, ...) plotSpatialRings(x, ...) )
+
+setMethod("coordinates", "Ring", function(obj) obj@coords)
+setMethod("coordinates", "SpatialRings", function(obj) lapply(obj@rings, coordinates))
+
+
+#if (!isGeneric("lines"))
+#    setGeneric("lines", function(x, y, ...)
+#        standardGeneric("lines"))
+
+#setMethod("lines", "Ring", function(x, y = NULL, ...) invisible(lines(coordinates(x), ...)))
+#setMethod("lines", "SpatialRings", function(x, y = NULL, ...) {
+#    f = function(x, ...) lines(x, ...)
+#    invisible(lapply(x@rings, f, ...))
+#})
+
+setMethod("row.names", "SpatialRings", function(x) sapply(slot(x, "rings"), slot, "ID"))
+setReplaceMethod("row.names", signature(x = "SpatialRings", value = "character"),
+                 function(x, value) spChFIDs(x, value))
+
+setMethod("[", "SpatialRings", 
+    function(x, i, j, ..., drop = TRUE) {
+        if (any(is.na(i))) stop("NAs not permitted in row index")
+        
+        if (is.logical(i)) {
+            if (length(i) == 1 && i) {
+                i = 1:length(x@rings)
+            } else {
+                i <- which(i)
+            }
+        } else if (is.character(i)) {
+            i <- match(i, row.names(x))
+        }
+        
+        x@rings = x@rings[i]
+        x@bbox = .bboxSRs(x@rings)
+        return(x)
+    }
+)
+
+
+setMethod("coordnames", signature(x = "SpatialRings"), function(x) coordnames(x@rings[[1]]))
+setMethod("coordnames", signature(x = "Ring"), function(x) dimnames(coordinates(x))[[2]])
+
+setReplaceMethod("coordnames", 
+                 signature(x = "SpatialRings", value = "character"),
+                 function(x, value) {
+                     dimnames(x@bbox)[[1]] = value
+                     for (i in seq(along = x@rings))
+                        coordnames(x@rings[[i]]) = value
+                     return(x)
+                 }
+)
+setReplaceMethod("coordnames",signature(x = "Ring", value = "character"),
+                 function(x, value) {
+                     dimnames(x@coords)[[2]] = value
+                     return(x)
+                 }
+)
+
+
+
+chFIDsSpatialRings <- function(obj, x) {
+    nl <- length(slot(obj, "rings"))
+    if (length(x) != nl) stop("lengths differ")
+    if (length(x) > length(unique(x))) stop("duplicate IDs")
+
+    rings <- slot(obj, "rings")
+    for (i in 1:nl) obj@rings[[i]]@ID = x[i]
+    
+    return(obj)
+}
+setMethod("spChFIDs", signature(obj="SpatialRings", x="character"), chFIDsSpatialRings)
+
+setAs("Ring", 
+      "SpatialPoints",
+      function(from) { 
+          SpatialPoints(do.call("rbind", coordinates(from)))
+      }
+)
+
+setAs("SpatialRings", 
+      "SpatialPoints", 
+      function(from) { 
+          SpatialPoints(do.call("rbind", 
+                        lapply(from@rings, function(x) as(x, "SpatialPoints"))),
+                        CRS(proj4string(from)))
+      }
+)
---
+++ pkg/R/rgeos_SpatialRingsDataFrame.R
@@ -0,0 +1,93 @@
+setClass("SpatialRingsDataFrame",
+    representation("SpatialRings", data = "data.frame"),
+    validity = function(object) {
+        if (!inherits(object@data, "data.frame"))
+            stop("data should be of class data.frame")
+        if (nrow(object@data) != length(object@rings))
+            stop("number of rows in data.frame and SpatialRings don't match")
+        return(TRUE)
+    }
+)
+
+SpatialRingsDataFrame = function(sr, data, match.ID = TRUE) {
+    if (match.ID) {
+        sr_IDs <- row.names(sr)
+        data_IDs <- row.names(data)
+        mtch <- match(sr_IDs, data_IDs)
+        if (any(is.na(mtch)))
+            stop("row.names of data and Rings IDs do not match")
+        if (length(unique(mtch)) != length(sr_IDs))
+            stop("row.names of data and Rings IDs do not match")
+        data <- data[mtch, , drop=FALSE]
+    }
+    if (nrow(data) != length(sr@rings))
+        stop("length of data.frame does not match number of Ring elements")
+    
+    return( new("SpatialRingsDataFrame", sr, data = data) )
+}
+
+setAs("SpatialRingsDataFrame","SpatialRings", function(from) SpatialRings(from@rings))
+setAs("SpatialRingsDataFrame", "data.frame", function(from) from@data)
+
+setMethod("names","SpatialRingsDataFrame", function(x) names(x@data))    
+setReplaceMethod("names", signature(x = "SpatialRingsDataFrame", value = "character"),
+                 function(x, value) { names(x@data)<-value; x })
+
+setMethod("row.names","SpatialRingsDataFrame", function(x) sapply(slot(x, "rings"), slot, "ID"))    
+setReplaceMethod("row.names", signature(x = "SpatialRingsDataFrame", value = "character"),
+              function(x, value) spChFIDs(x, value))
+
+
+setMethod("[", c("SpatialRingsDataFrame", "ANY", "ANY"), 
+    function(x, i, j, ... , drop = TRUE) {
+        missing.i = missing(i)
+        missing.j = missing(j)
+        nargs = nargs() # e.g., a[3,] gives 2 for nargs, a[3] gives 1.
+        
+        if (missing.i && missing.j) {
+            i = TRUE
+            j = TRUE
+        } else if (missing.j && !missing.i) {
+            if (nargs == 2) {
+                j = i
+                i = TRUE
+            } else {
+                j = TRUE
+            }
+        } else if (missing.i && !missing.j) {
+            i = TRUE
+        }
+        
+        if (is.matrix(i)) stop("matrix argument not supported in SpatialRingsDataFrame selection")
+        if (is.logical(i)) {
+            if (length(i) == 1 && i)
+                i = 1:length(x@rings)
+            else
+                i = which(i)
+        } else if (is.character(i)) {
+                i = match(i, row.names(x))
+        }
+        
+        if (any(is.na(i))) stop("NAs not permitted in row index")
+        
+        x@rings = x@rings[i]
+        x@data = x@data[i, j, ..., drop = FALSE]
+        x@bbox = .bboxSRs(x@rings)
+        
+        return(x)
+    }
+)
+
+#setMethod("lines","SpatialRingsDataFrame", function(x, y = NULL, ...) lines(as(x, "SpatialRings"), ...))    
+setMethod("dim","SpatialRingsDataFrame", function(x) dim(x@data))
+
+
+chFIDsSpatialRingsDataFrame <- function(obj, x) {
+    SR <- as(obj, "SpatialRings")
+    SRx <- spChFIDs(SR, x)
+    df <- as(obj, "data.frame")
+    row.names(df) <- sapply(slot(SLx, "rings"), function(x) slot(x, "ID"))
+    SpatialRingsDataFrame(SLx, data=df)
+}
+
+setMethod("spChFIDs", signature(obj="SpatialRingsDataFrame", x="character"), chFIDsSpatialRingsDataFrame)
