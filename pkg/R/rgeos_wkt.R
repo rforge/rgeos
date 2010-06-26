116666af7fd92b2f07982d29072f5c92d0a4afb3
crundel <crundel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-26 18:41:32 +0000

    changed readWKT, let GEOS do all parsing/checking of WKTs instead of trying to sort it out in R.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgeos@117 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rgeos_wkt.R
+++ pkg/R/rgeos_wkt.R
@@ -1,80 +1,23 @@
 
-#TODO - this should probably be more robust
-isWKT = function( s ) {
-    
-    s = str_trim(s)
-    validWKT = c("^POINT", "^LINESTRING", "^LINEARRING", "^POLYGON", "^MULTIPOINT", 
-                 "^MULTILINESTRING", "^MULTIPOLYGON", "^GEOMETRYCOLLECTION")
-    
-    return(any( sapply(validWKT, function(x) str_detect(s,x)) ))
-}
-
-
-# TODO - could be done more efficiently with regexs
-cleanWKT = function( text ) {
-    
-    text = str_replace(str_trim(text),"\n","")
-
-    openPos  = c( gregexpr("\\(", text)[[1]], nchar(text)+1)
-    closePos = c( gregexpr("\\)", text)[[1]], nchar(text)+1)
-
-    i = 1
-    j = 1
-    splitPos = c()
-    count = 0
-    while ( i < length(openPos) | j < length(closePos) ) {
-        if (openPos[i] < closePos[j]) {
-            count = count+1
-            i = i+1
-        } else {
-            count = count-1
-            j = j+1
-        }
-    
-        if (count < 0) stop("invalid WKT text, unbalanced parenthesis")
-        
-        if (count == 0) splitPos = c(splitPos, closePos[j-1])
-    }
-    
-    if (count != 0 ) stop("invalid WKT text, unbalanced parenthesis")
-    
-    startPos = c(1, splitPos[-length(splitPos)]+1)
-    
-    rep = str_trim( substring(text,startPos,splitPos) )
-    
-    validWKTs = sapply(rep,isWKT)
-    if ( !all(validWKTs) ) {
-        warning(paste("cleanWKT: following WKT strings are invalid, ignoring.\n",
-                      paste( rep[validWKTs],collapse="\n" ) ))
-    }
-    
-    return( rep[validWKTs] )
-}
-
-
 readWKT = function( text, id = NULL, p4s = NULL, threshold=0) {
     
-    wkts = cleanWKT(text)
-    
-    m =  str_match_all(wkts, "POINT|LINESTRING|LINEARRING|POLYGON|MULTIPOINT|MULTILINESTRING|MULTIPOLYGON")   
-    ngeoms =  sapply(m, function(x) dim(x)[1] )
-    
-    if(is.null(id)) id = 1:sum(ngeoms)
+    #WKTregex = "(GEOMETRYCOLLECTION\\s?\\(?\\s?)?(((POINT|LINESTRING|LINEARRING|POLYGON|MULTIPOINT|MULTILINESTRING|MULTIPOLYGON)\\s?((\\(\\s?(\\(?\\s?(\\(?((-?\\d+(\\.\\d+)?\\s-?\\d+(\\.\\d+)?,?\\s?)+,?)+\\)?,?\\s?)+\\)?,?\\s?)+\\))|EMPTY),?\\s?)+)\\)?"
+    #wkts = str_match_all(text,WKTregex)[[1]][,1]
+        
+    wkt = str_replace(text,"\n","")
 
-    # if the number of ids doesn't take into account geometry collections create subids
-    if( length(id) == length(wkts)) {
-        newid = c()
-        for( i in 1:length(wkts) ) {
-            if(ngeoms[i] == 1) {
-                newid = c(newid, id[i])
-            } else {
-                newid = c(newid, paste(id[i],1:ngeoms[i],sep=".") )
-            }
-        }
-        id = newid
-    }
+    if (length(wkt) != 1) stop("WKT must have length 1")
+    
+    m =  str_match_all(wkt, "POINT|LINESTRING|LINEARRING|POLYGON|MULTIPOINT|MULTILINESTRING|MULTIPOLYGON")   
+    ngeoms =  dim( m[[1]] )[1]
+    
+    if(is.null(id)) id = 1:ngeoms
 
-    if( length(id) != sum(ngeoms) )
+    # if the number of ids doesn't take into account sub geometries in geometry collection then create subids
+    if( length(id) == 1 & ngeoms != 1)
+        id = paste(id,1:ngeoms,sep=".")
+    
+    if( length(id) != ngeoms )
         stop("number of WKT geometries does not match number of ids")        
 
     p4s = checkP4S(p4s)
@@ -82,14 +25,13 @@ readWKT = function( text, id = NULL, p4s = NULL, threshold=0) {
     threshold=as.double(threshold)
     id = as.character(id)
         
-    res = list()
-    for(i in 1:length(wkts) ) {
-        ids = id[ sum( ngeoms[1:i] )-ngeoms[i]+1:ngeoms[i] ]
-        res[[i]] <- .Call("rgeos_readWKT", .RGEOS_HANDLE, wkts[i], p4s, ids, threshold, PACKAGE="rgeos")
-    }
-    if(length(res) == 1) {
-        res <- res[[1]]
-    }
+    tryCatch(res <- .Call("rgeos_readWKT", .RGEOS_HANDLE, wkt, p4s, 
+                            id, threshold, PACKAGE="rgeos"), 
+             error = function(e) { stop( paste( "Unable to parse: ",wkt,"\n",
+                                                "GEOS reported: \"", e$message,"\"",sep=""),call.=FALSE) } )
+    
+    if (length(unique(row.names(res))) != ngeoms)
+        warning("Number of geometries does not match between object and text, check WKT validity.",call.=FALSE)
     
     return( res )
 }
