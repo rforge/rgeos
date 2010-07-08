d7a30812a3ccb7ec3a07e7ca6c8b824bda0eb062
crundel <crundel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-08 20:17:34 +0000

    Updated unit tests for handling empty geometries (added polygons and a few small fixes)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgeos@129 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/tests/test-empty-geom.R
+++ pkg/inst/tests/test-empty-geom.R
@@ -76,6 +76,14 @@ test_that("empty linestrings", {
     expect_that( l3, is_identical_to(doubletranslate(l3)) )
     expect_that( l4, is_identical_to(doubletranslate(l4)) )
     
+    
+    ml1 = readWKT("MULTILINESTRING((1 1,2 2), EMPTY)")
+    ml1 = readWKT("MULTILINESTRING(EMPTY, (1 1,2 2))")
+    
+    expect_that( ml1, is_identical_to(doubletranslate(ml1)) )
+    expect_that( ml2, is_identical_to(doubletranslate(ml2)) )
+    
+    
     lg1 = readWKT("GEOMETRYCOLLECTION(LINESTRING EMPTY,LINESTRING EMPTY)")
     lg2 = readWKT("GEOMETRYCOLLECTION(MULTILINESTRING EMPTY,MULTILINESTRING EMPTY)")
     lg3 = readWKT("GEOMETRYCOLLECTION(LINESTRING EMPTY,MULTILINESTRING EMPTY)")
@@ -135,5 +143,72 @@ test_that("empty linearrings", {
     expect_that( lrg1, is_identical_to(doubletranslate(lrg1)) )
     expect_that( lrg2, is_identical_to(doubletranslate(lrg2)) )
     expect_that( lrg3, is_identical_to(doubletranslate(lrg3)) )
+
+})
+
+
+test_that("empty polygons", {
+
+    p1 = readWKT("POLYGON EMPTY")
+    p2 = readWKT("MULTIPOLYGON EMPTY")
+    p3 = readWKT("GEOMETRYCOLLECTION(POLYGON EMPTY)")
+    p4 = readWKT("GEOMETRYCOLLECTION(MULTIPOLYGON EMPTY)")
+    
+    expect_that( p1, is_identical_to(p2) )
+    expect_that( p2, is_identical_to(p3) )
+    expect_that( p3, is_identical_to(p4) )
+    
+    expect_that( p1, is_identical_to(doubletranslate(p1)) )
+    expect_that( p2, is_identical_to(doubletranslate(p2)) )
+    expect_that( p3, is_identical_to(doubletranslate(p3)) )
+    expect_that( p4, is_identical_to(doubletranslate(p4)) )
+    
+    
+    mp1 = readWKT("MULTIPOLYGON(((1 1,2 2,3 1,1 1)), EMPTY)")
+    mp2 = readWKT("MULTIPOLYGON(EMPTY, ((1 1,2 2,3 1,1 1)))")
+    
+    expect_that( mp1, is_identical_to(doubletranslate(mp1)) )
+    expect_that( mp2, is_identical_to(doubletranslate(mp2)) )
+    
+    
+    pg1 = readWKT("GEOMETRYCOLLECTION(POLYGON EMPTY,POLYGON EMPTY)")
+    pg2 = readWKT("GEOMETRYCOLLECTION(POLYGON EMPTY,MULTIPOLYGON EMPTY)")
+    pg3 = readWKT("GEOMETRYCOLLECTION(MULTIPOLYGON EMPTY,POLYGON EMPTY)")
+    pg4 = readWKT("GEOMETRYCOLLECTION(MULTIPOLYGON EMPTY,MULTIPOLYGON EMPTY)")
+    
+    pg5 = readWKT("GEOMETRYCOLLECTION(POLYGON EMPTY, POLYGON((1 1,2 2,3 1,1 1)) )")
+    pg6 = readWKT("GEOMETRYCOLLECTION(MULTIPOLYGON EMPTY, POLYGON((1 1,2 2,3 1,1 1)) )")
+    pg7 = readWKT("GEOMETRYCOLLECTION(POLYGON EMPTY, MULTIPOLYGON(((1 1,2 2,3 1,1 1))) )")
+    pg8 = readWKT("GEOMETRYCOLLECTION(MULTIPOLYGON EMPTY, MULTIPOLYGON(((1 1,2 2,3 1,1 1))) )")
+
+    pg9 = readWKT("GEOMETRYCOLLECTION(POLYGON((1 1,2 2,3 1,1 1)), POLYGON EMPTY)")
+    pg10= readWKT("GEOMETRYCOLLECTION(POLYGON((1 1,2 2,3 1,1 1)), MULTIPOLYGON EMPTY)")
+    pg11= readWKT("GEOMETRYCOLLECTION(MULTIPOLYGON(((1 1,2 2,3 1,1 1))), POLYGON EMPTY)")
+    pg12= readWKT("GEOMETRYCOLLECTION(MULTIPOLYGON(((1 1,2 2,3 1,1 1))), MULTIPOLYGON EMPTY)")
+
+    expect_that( pg1, is_identical_to(pg2) )
+    expect_that( pg2, is_identical_to(pg3) )
+    expect_that( pg3, is_identical_to(pg4) )
+    
+    expect_that( pg5, is_identical_to(pg6) )
+    expect_that( pg6, is_identical_to(pg7) )
+    expect_that( pg7, is_identical_to(pg8) )
+
+    expect_that( pg9, is_identical_to(pg10) )
+    expect_that(pg10, is_identical_to(pg11) )
+    expect_that(pg11, is_identical_to(pg12) )
+
+    expect_that( pg1, is_identical_to(doubletranslate(pg1)) )
+    expect_that( pg2, is_identical_to(doubletranslate(pg2)) )
+    expect_that( pg3, is_identical_to(doubletranslate(pg3)) )
+    expect_that( pg4, is_identical_to(doubletranslate(pg4)) )
+    expect_that( pg5, is_identical_to(doubletranslate(pg5)) )
+    expect_that( pg6, is_identical_to(doubletranslate(pg6)) )
+    expect_that( pg7, is_identical_to(doubletranslate(pg7)) )
+    expect_that( pg8, is_identical_to(doubletranslate(pg8)) )
+    expect_that( pg9, is_identical_to(doubletranslate(pg9)) )
+    expect_that( pg10,is_identical_to(doubletranslate(pg10)))
+    expect_that( pg11,is_identical_to(doubletranslate(pg11)))
+    expect_that( pg12,is_identical_to(doubletranslate(pg12)))
 
 })
