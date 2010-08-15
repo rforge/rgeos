dde6f079d5712af8d469b259d9beaa966176bd9e
crundel <crundel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-15 10:05:29 +0000

    Cleaned up code / variable declarations
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgeos@186 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/rgeos_predicate_unary.c
+++ pkg/src/rgeos_predicate_unary.c
@@ -24,43 +24,33 @@ SEXP rgeos_isempty(SEXP env, SEXP spgeom, SEXP byid) {
 SEXP rgeos_unarypredfunc(SEXP env, SEXP spgeom, SEXP byid, 
                        char (*unarypredfunc)(GEOSContextHandle_t, const GEOSGeom)) {
 
-    SEXP ans;
-    GEOSGeom geom, curgeom;
-    
-    int val;
-    int i, n, pc=0;
-    int type;
-
     GEOSContextHandle_t GEOShandle = getContextHandle(env);
     
-    geom = rgeos_convert_R2geos(env, spgeom);
-    type = GEOSGeomTypeId_r(GEOShandle, geom);
+    GEOSGeom geom = rgeos_convert_R2geos(env, spgeom);
+    int type = GEOSGeomTypeId_r(GEOShandle, geom);
 
-    n = 1;
-    if (LOGICAL_POINTER(byid)[0] && type == GEOS_GEOMETRYCOLLECTION)
-        n = GEOSGetNumGeometries_r(GEOShandle, geom);
+    int n = (LOGICAL_POINTER(byid)[0] && type == GEOS_GEOMETRYCOLLECTION) ?
+			GEOSGetNumGeometries_r(GEOShandle, geom) : 1;
 
     if (n == -1) error("rgeos_unarypredfunc: invalid number of subgeometries");
     
+	SEXP ans;
+	int pc=0;
     PROTECT(ans = NEW_LOGICAL(n)); pc++;
 
-    curgeom = geom;
-    for(i=0; i<n; i++) {
-        if ( n > 1) {
-            curgeom = (GEOSGeom) GEOSGetGeometryN_r(GEOShandle, geom, i);
-            if (curgeom == NULL) 
-                error("rgeos_unarypredfunc: unable to get subgeometries");
-        }
+    for(int i=0; i<n; i++) {
+        
+		GEOSGeom curgeom = (n > 1) ? (GEOSGeom) GEOSGetGeometryN_r(GEOShandle, geom, i) : geom;
 
-        val = (int) unarypredfunc(GEOShandle, curgeom);
-        if (val == 2)
-            error("rgeos_unarypredfunc: test failed");
+        if (curgeom == NULL) error("rgeos_unarypredfunc: unable to get subgeometries");
+
+        int val = (int) unarypredfunc(GEOShandle, curgeom);
+        
+		if (val == 2) error("rgeos_unarypredfunc: test failed");
 
         LOGICAL_POINTER(ans)[i] = val;
     }
 
-    GEOSGeom_destroy_r(GEOShandle, geom);
-    
     UNPROTECT(pc);
     return(ans);
 }
