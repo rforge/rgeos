fcf188ceeacdcc8252b9b289f738f7354dc45173
rsbivand <rsbivand@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-30 16:42:43 +0000

    update web page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgeos@245 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -25,12 +25,12 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 <h1>rgeos</h1>
 <p>
-The rgeos package is an R wrapper for the GEOS library. GEOS is a powerful open source geometry engine written in C++ that implements spatial functions and operators from the OpenGIS Simple Features for SQL specification. rgeos will make these tools available within R and will integrate with existing spatial data tools through the sp package.
+The rgeos package is an R wrapper for the GEOS library. GEOS is a powerful open source geometry engine written in C++ that implements spatial functions and operators from the OpenGIS Simple Features for SQL specification. rgeos makes these tools available within R and integrates with existing spatial data tools through the sp package.
 </p>
 
 <h1>Development</h1>
 <p>
-This project is currently under active development and should be considered to be in a pre-alpha state. 
+This project is currently under active development; it has been released to CRAN, and may be downloaded within R using install.packages(), or from the package <a href="http://cran.r-project.org/web/packages/rgeos/index.html">CRAN page</a>. 
 </p>
 
 <p> The <strong>project summary page</strong> can be found <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
